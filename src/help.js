30acd5516ec937532136b6e7a8b0eb4ee4a4fa52
Chris Troutner <chris.troutner@gmail.com>
2021-05-15 14:00:14 -0700

    Fixed broken dependencies

---
+++ src/help.js
@@ -0,0 +1,16 @@
+/*
+  Shows the help for new users.
+*/
+
+function help() {
+  console.log(`
+This is a program for making paper wallets. Available commands:
+
+npm start: Show this help file.
+npm run bch: Make BCH paper wallets.
+
+Each command assumes that you've opened it and customized the variables at the
+top of the file.
+`)
+}
+help()
