63983e782db841c577fd3e782c40d561dc2ee587
Chris Troutner <chris.troutner@gmail.com>
2021-05-15 14:12:18 -0700

    Adding command for slp paper wallets

--- src/help.js
+++ src/help.js
@@ -8,6 +8,7 @@ This is a program for making paper wallets. Available commands:
 
 npm start: Show this help file.
 npm run bch: Make BCH paper wallets.
+npm run slp: Make SLP token paper wallets.
 
 Each command assumes that you've opened it and customized the variables at the
 top of the file.
