65cebce00b6bd30b09762be11f3f2edaf11eecc1
Chris Troutner <chris.troutner@gmail.com>
2024-10-11 11:04:36 -0300

    Updating README

--- src/help.js
+++ src/help.js
@@ -7,9 +7,19 @@ function help () {
 This is a program for making paper wallets. Available commands:
 
 npm start: Show this help file.
-npm run bch: Make BCH paper wallets.
-npm run slp: Make SLP token paper wallets.
-npm run btc: Make BTC paper wallets.
+
+For generating individual paper wallets:
+npm run bch - Make 10 individual BCH paper wallets.
+npm run slp - Make 10 individual SLP token paper wallets.
+npm run btc - Make 10 individual BTC paper wallets.
+npm run eth - Make 10 individual ETH paper wallets.
+npm run xec - Make 10 individual XEC paper wallets.
+
+For generating a grid of paper wallets for bulk processing with a laser engraver:
+npm run bch:bulk - Generate a 3x5 grid of BCH wallets
+npm run btc:bulk - Generate a 3x5 grid of BTC wallets
+npmn run eth:bulk - Generate a 3x5 grid of ETH wallets
+
 
 Each command assumes that you've opened it and customized the variables at the
 top of the file.
